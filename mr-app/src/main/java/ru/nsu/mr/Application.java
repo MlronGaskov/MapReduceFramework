279f5864423d187ccd5378be117b515fef4aa4e5
Daria Lavitskaya <d.lavitskaya@g.nsu.ru>
2024-12-15 21:28:13 +0700

    [mr-43344713][app] Implemented application.

---
+++ mr-app/src/main/java/ru/nsu/mr/Application.java
@@ -0,0 +1,20 @@
+package ru.nsu.mr;
+
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage: java -jar Application.jar <path-to-config> <additional-args>");
+            System.exit(1);
+        }
+        String[] additionalArgs = Arrays.copyOfRange(args, 1, args.length);
+
+        ConfigurationLoader loader = new ConfigurationLoader("C:/MapReduceFiles/mrShit/config.yml");
+        JarFileParser jarFileParser = new JarFileParser(loader.getJarPath());
+        MapReduceJob<?, ?, ?, ?> job = jarFileParser.loadUsersSubClass(MapReduceJob.class);
+        Launcher.launch(job, loader.getConfig(), loader.getMappersOutputPath(), loader.getReducersOutputPath(),
+                loader.getInputFiles(), additionalArgs);
+    }
+}
