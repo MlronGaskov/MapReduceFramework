ea8ffa1de234ae3f340043a08bb580c8cca2da84
darialavitskaya <117648558+dariamukomel@users.noreply.github.com>
2024-12-15 21:00:10 +0700

    [mr-43178362][hotfix] Refactored yaml configuration loader

--- mr-core/src/main/java/ru/nsu/mr/ConfigurationLoader.java
+++ mr-core/src/main/java/ru/nsu/mr/ConfigurationLoader.java
@@ -5,84 +5,75 @@ import org.yaml.snakeyaml.Yaml;
 import ru.nsu.mr.config.Configuration;
 import ru.nsu.mr.config.ConfigurationOption;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ConfigurationLoader {
-    private Configuration config = new Configuration();
-    private Path mappersOutputPath;
-    private Path reducersOutputPath;
+    private Configuration configuration = new Configuration();
+    private Path mapperOutputDirectory;
+    private Path outputDirectory;
     private Path jarPath;
     private List<Path> inputFiles = new ArrayList<>();
 
+    private static class Config {
+        public String jarPath;
+        public String inputDirectory;
+        public String mapperOutputDirectory;
+        public String outputDirectory;
+        public String metricsPort;
+        public String logsPath;
+        public Integer mappersCount;
+        public Integer reducersCount;
+        public Integer sorterInMemoryRecords;
+    }
+
     public ConfigurationLoader(String filePath) throws IOException {
+
         // Создаем экземпляр LoaderOptions
         LoaderOptions options = new LoaderOptions();
 
         // Создаем экземпляр Yaml с LoaderOptions
-        Yaml yaml = new Yaml(options);// Создаем экземпляр Yaml с конструктором для Map
-        try (FileInputStream fileInputStream = new FileInputStream(new File(filePath))) {
-            // Считываем данные из YAML файла и сохраняем в Map
-            Map<String, Object> yamlValues = yaml.load(fileInputStream);
+        Yaml yaml = new Yaml(options);
 
-            for (Map.Entry<String, Object> entry : yamlValues.entrySet()) {
-                String key = entry.getKey();
-                Object value = entry.getValue();
 
-                switch (key) {
-                    case "mappersCount":
-                        config.set(ConfigurationOption.MAPPERS_COUNT, (Integer) value);
-                        break;
-                    case "reducersCount":
-                        config.set(ConfigurationOption.REDUCERS_COUNT, (Integer) value);
-                        break;
-                    case "memoryRecords":
-                        config.set(ConfigurationOption.SORTER_IN_MEMORY_RECORDS, (Integer) value);
-                        break;
-                    case "metricsPort":
-                        config.set(ConfigurationOption.METRICS_PORT, (String) value);
-                        break;
-                    case "workersCount":
-                        config.set(ConfigurationOption.WORKERS_COUNT, (Integer) value);
-                        break;
-                    case "mapperOutputDirectory":
-                        this.mappersOutputPath = Path.of((String) value);
-                        break;
-                    case "outputDirectory":
-                        this.reducersOutputPath = Path.of((String) value);
-                        break;
-                    case "jarPath":
-                        this.jarPath = Path.of((String) value);
-                        break;
-                    case "inputDirectory":
-                        Path inputPath = Path.of((String) value);
-                        if (Files.isDirectory(inputPath)) {
-                            Files.list(inputPath).forEach(inputFiles::add);
-                        } else if (Files.isRegularFile(inputPath)) {
-                            inputFiles.add(inputPath);
-                        }
-                        break;
-                }
-            }
+        Config config;
+        try (InputStream inputStream = Files.newInputStream(Path.of(filePath))) {
+            config = yaml.loadAs(inputStream, Config.class);
         }
+
+        jarPath = Path.of(config.jarPath);
+        mapperOutputDirectory = Path.of(config.mapperOutputDirectory);
+        outputDirectory = Path.of(config.outputDirectory);
+        configuration.set(ConfigurationOption.METRICS_PORT, config.metricsPort)
+                .set(ConfigurationOption.MAPPERS_COUNT, config.mappersCount)
+                .set(ConfigurationOption.REDUCERS_COUNT, config.reducersCount)
+                .set(ConfigurationOption.LOGS_PATH, config.logsPath);
+        if (config.sorterInMemoryRecords != null) {
+            configuration.set(ConfigurationOption.SORTER_IN_MEMORY_RECORDS, config.sorterInMemoryRecords);
+        }
+        Path inputPath = Path.of(config.inputDirectory);
+        if (Files.isDirectory(inputPath)) {
+            Files.list(inputPath).forEach(inputFiles::add);
+        } else if (Files.isRegularFile(inputPath)) {
+            inputFiles.add(inputPath);
+        }
+
     }
 
     public Configuration getConfig() {
-        return config;
+        return configuration;
     }
 
     public Path getMappersOutputPath() {
-        return mappersOutputPath;
+        return mapperOutputDirectory;
     }
 
     public Path getReducersOutputPath() {
-        return reducersOutputPath;
+        return outputDirectory;
     }
 
     public Path getJarPath() {
